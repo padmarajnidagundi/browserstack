217f38f6ab84273476363e9a734bce31228e14a8
Padmaraj Nidagundi <padmaraj.nidagundi@gmail.com>
2017-08-26 14:33:53 +0300

    JUNIT youtube automation

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,8 +1,12 @@
-import java.util.concurrent.TimeUnit;
 import junit.framework.TestCase;
-import org.junit.*;
-import org.openqa.selenium.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Test1 extends TestCase {
     private WebDriver driver;
@@ -15,7 +19,7 @@ public class Test1 extends TestCase {
                 "chromedriver.exe");
         driver = new ChromeDriver();
         baseUrl = "https://www.youtube.com/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     }
 
     @Test
